90ce1fe7599d7a9fd2f605c991b9c2df7f8d7f29
JustinMutinta <justinmutinta@hotmail.com>
2021-07-28 13:50:09 -0500

    Added label with current time. Working on getting it to update in real time

--- src/clock.java
+++ src/clock.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class clock extends JFrame {
     public clock(){
@@ -24,6 +26,11 @@ public class clock extends JFrame {
         add(panel1);
         add(label2);
 
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        JLabel label3 = new JLabel(dtf.format(now));
+        add(label3);
 
         setVisible(true); //needs to be last.
 
