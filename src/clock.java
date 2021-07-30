f65c7e0475b35493bdf4e3bc174c53f958457525
JustinMutinta <justinmutinta@hotmail.com>
2021-07-30 14:15:17 -0500

    Third working version. Working on Background colors

--- src/clock.java
+++ src/clock.java
@@ -7,7 +7,9 @@ public class clock extends JFrame{
     //JFrame timeFrame;
     JLabel timeLabel;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
     String time;
+    String day;
 
     public clock(){
 
@@ -15,11 +17,15 @@ public class clock extends JFrame{
         setTitle("Clock");
         setLayout(new FlowLayout());
         setSize(500,100);
+        getContentPane().setBackground(Color.BLACK);   // to change background of JFram
 
         timeFormat = new SimpleDateFormat("HH:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
 
         timeLabel = new JLabel();
         timeLabel.setFont(new Font("times roman plain",Font.BOLD,50));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setOpaque(true);
 
         add(timeLabel);
 
@@ -35,7 +41,8 @@ public class clock extends JFrame{
     public void setTime(){
         while(true){
             time = timeFormat.format(Calendar.getInstance().getTime());
-            timeLabel.setText(time);
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(day + " " + time);
 
             try {
                 Thread.sleep(1000);
