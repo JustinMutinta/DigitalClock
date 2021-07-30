535ca885168f75f8a7ebe36ecad3c7c6ef77afae
JustinMutinta <justinmutinta@hotmail.com>
2021-07-30 12:25:21 -0500

    Cleaned out a lot of code/functions I did not need

--- src/clock.java
+++ src/clock.java
@@ -3,28 +3,20 @@ import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class clock extends JFrame {
+public class clock extends JFrame{
     public clock(){
         super("Clock");
-        setLookAndFeel();
+
         setSize(700,150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         FlowLayout flo = new FlowLayout();
         setLayout(flo);
 
-        JButton button1 = new JButton("Test");
-        add(button1);
-
-        JLabel label1 = new JLabel("Test Label1");
-        JLabel label2 = new JLabel("Test label2");
-
         JPanel panel1 = new JPanel();
         panel1.setSize(50,50);
-        panel1.add(label1);
 
         add(panel1);
-        add(label2);
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
@@ -36,15 +28,11 @@ public class clock extends JFrame {
 
     }
 
-    private void setLookAndFeel(){
-        try{
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        }catch(Exception exc){
-            //ignore error
-        }
-    }
 
-    public static void main(String[] args) {
-        clock newClock = new clock();
+
+    public static void main(String[] args){
+
+            clock newClock = new clock();
+
     }
 }
